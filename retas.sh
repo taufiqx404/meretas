d561734834c2d915f4cbad5a71cc2ae63263798d
taufiqx404 <59577411+taufiqx404@users.noreply.github.com>
2020-08-24 18:41:11 +0800

    Update and rename README.md to retas.sh

---
+++ retas.sh
@@ -0,0 +1,169 @@
+# meretas
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+#jangan di recode ya bang
+echo
+echo
+echo
+echo "╔╗─╔╗╔═══╗╔═══╗╔╗╔═╗╔═══╗╔═══╗"
+echo "║║─║║║╔═╗║║╔═╗║║║║╔╝║╔══╝║╔═╗║"
+echo "║╚═╝║║║─║║║║─╚╝║╚╝╝─║╚══╗║╚═╝║"
+echo "║╔═╗║║╚═╝║║║─╔╗║╔╗║─║╔══╝║╔╗╔╝"
+echo "║║─║║║╔═╗║║╚═╝║║║║╚╗║╚══╗║║║╚╗"
+echo "╚╝─╚╝╚╝─╚╝╚═══╝╚╝╚═╝╚═══╝╚╝╚═╝"
+echo $i"SEDANG MENUJU TOOLS"
+sleep 2
+echo $me"▶ ▶ ▶ ▶ ▶ ▶ ▶ ▶ ▶ ▶ |100%"
+sleep 1
+echo $ku"LOGIN" $me"SUKSES"
+sleep 1
+clear
+echo $cy" █   █ █▀▀ █   ▄▀ ▄▀▄ █▄ ▄█ █▀▀"
+echo $i" █ █ █ █▀▀ █ ▄ █  █ █ █ █ █ █▀▀"
+echo $me"  ▀ ▀  ▀▀▀ ▀▀▀  ▀  ▀  ▀   ▀ ▀▀▀"
+echo $i"     GUNAKAN DENGAN BIJAK"
+sleep 2
+clear
+echo $me"╔════╗╔═══╗╔╗─╔╗╔═══╗╔══╗╔═══╗"
+echo $me"║╔╗╔╗║║╔═╗║║║─║║║╔══╝╚╣─╝║╔═╗║"
+echo $me"╚╝║║╚╝║║─║║║║─║║║╚══╗─║║─║║─║║"
+echo $ku"──║║──║╚═╝║║║─║║║╔══╝─║║─║╚═╝║"
+echo $ku"──║║──║╔═╗║║╚═╝║║║───╔╣─╗╚══╗║"
+echo $ku"──╚╝──╚╝─╚╝╚═══╝╚╝───╚══╝───╚╝"
+sleep 1
+echo $me"        ╔═╗╔═╗"
+echo $me"        ╚╗╚╝╔╝"
+echo $ku"        ─╚╗╔╝─"
+echo $ku"        ─╔╝╚╗─"
+echo $i"        ╔╝╔╗╚╗"
+echo $i"        ╚═╝╚═╝"
+sleep 2
+clear
+echo $me"╔════╗╔═══╗╔═══╗╔═╗╔═╗╔╗─╔╗╔═╗╔═╗"
+echo $me"║╔╗╔╗║║╔══╝║╔═╗║║║╚╝║║║║─║║╚╗╚╝╔╝"
+echo $me"╚╝║║╚╝║╚══╗║╚═╝║║╔╗╔╗║║║─║║─╚╗╔╝─"
+echo $i"──║║──║╔══╝║╔╗╔╝║║║║║║║║─║║─╔╝╚╗─"
+echo $i"──║║──║╚══╗║║║╚╗║║║║║║║╚═╝║╔╝╔╗╚╗"
+echo $i"──╚╝──╚═══╝╚╝╚═╝╚╝╚╝╚╝╚═══╝╚═╝╚═╝"
+echo $pu"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo $me"▌       PILIH TOOLS            ▌"
+echo $pu"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo $bi"♥☞" 1"$i" "$i TEBAS INDEX"
+echo $me"♥☞" 2"$me" "$me DDOS TORJAN"
+echo $cy"♥☞" 3"$cy" "$cy INSTALLER"
+echo $pr"♥☞" 4"$bi" "$bi IP SCAN"
+echo $ku"♥☞" 5"$ku" "$ku VIRUS"
+echo $me"♥☞" 6"$me" "$me DDOS BLACK HAT"
+echo $bi"♥☞" 7"$me" "$pr INSTALL BAHAN"
+echo $pu"♥☞" 8"$cy" "$me EXIT"
+echo $pu"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo $me"▬▬▬▬▬▬▬"$me"【"$me"RED DEVIL OFF"$me"】"$i"▬▬▬▬▬▬▬"$i"【"$i""pilih tools""$i"】"
+echo $ku"▌"
+read -p"▬▬▬▬▬▬▬▶" pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant " TUNGGU "|lolcat
+sleep 1
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+bash install.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant " TUNGGU "|lolcat
+sleep 1
+git clone https://github.com/MrTamfanX/MrDdos
+cd MrDdos
+sh tamfan-ddos.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant " TUNGGU "|lolcat
+sleep 1
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+sh TUANB4DUT.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant " TUNGGU "|lolcat
+sleep 1
+git clone https://github.com/Bl4ckDr460n/ScanIp
+cd ScanIp
+python2 scanip.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant " TUNGGU "|lolcat
+sleep 1
+git clone https://github.com/saydog/Virus-droid-creator
+cd Virus-droid-creator
+python2 dog.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant " TUNGGU "|lolcat
+sleep 1
+git clone https://github.com/BHC-FAKE/BlackHatAttack
+cd BlackHatAttack
+fi
+
+if [ $pil = 7 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant "B E R H A S I L"|lolcat
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo "$pu▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "$me ╔════╗╔╗─╔╗╔═══╗╔═╗─╔╗╔╗╔═╗╔═══╗"
+echo "$me ║╔╗╔╗║║║─║║║╔═╗║║║╚╗║║║║║╔╝║╔═╗║"
+echo "$me ╚╝║║╚╝║╚═╝║║║─║║║╔╗╚╝║║╚╝╝─║╚══╗"
+echo "$bi ──║║──║╔═╗║║╚═╝║║║╚╗║║║╔╗║─╚══╗║"
+echo "$bi ──║║──║║─║║║╔═╗║║║─║║║║║║╚╗║╚═╝║"
+echo "$bi ──╚╝──╚╝─╚╝╚╝─╚╝╚╝─╚═╝╚╝╚═╝╚═══╝"
+echo "$pu▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo $cy"THANKS YA TOL"
+sleep 2
+echo $i"RED DEVIL OFF"
+sleep 2
+echo $pur" OKE SEKIAN GAN"
+exit
+fi
